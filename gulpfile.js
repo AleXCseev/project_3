c21375802d776b635e3cc99022eb35e2a44d2ec6
Alekseev Konstantin <alexcseev1990@gmail.com>
2021-03-21 18:46:51 +0200

    add review

--- gulpfile.js
+++ gulpfile.js
@@ -14,6 +14,7 @@ const styleFiles = [
 	"./src/css/first-section.scss",
 	"./src/css/second-section.scss",
 	"./src/css/sprey-section.scss",
+	"./src/css/third-section.scss",
 	"./src/css/reviews.scss",
 	"./src/css/footer.scss",
 	"./src/css/sliders.scss",
--- src/js/lib.js
+++ src/js/lib.js
@@ -1,4 +1,8 @@
 $(function () {
-
+	$('.reviews__wrapper').slick({
+		dots: true,
+		slidesToShow: 1,
+		slideToScroll: 1,
+	});
 })
 
