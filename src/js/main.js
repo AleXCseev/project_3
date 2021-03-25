ae5155cd05c27fa373f4405c7901554419924b97
Alekseev Konstantin <alexcseev1990@gmail.com>
2021-03-25 10:55:14 +0200

    finish

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,119 @@
 $(function () {
+	AOS.init();
+
+	$('[data-fancybox]').fancybox({
+		loop: true,
+	});
+
+	$('.reviews__wrapper').slick({
+		dots: true,
+		slidesToShow: 1,
+		slideToScroll: 1,
+		variableWidth: true,
+		centerMode: true,
+		arrows: false,
+		autoplay: true,
+		autoplaySpeed: 4000,
+	});
+
+	$.raty.path = 'img/raty';
+
+	$('.modal__raiting').raty({
+		half: true,
+		space: false,
+		number: 5,
+	});
+
+	function modal() {
+		$(".reviews .order__link").click(function () {
+			$.fancybox.open($(".modal"), {
+				infobar: false,
+				touch: false,
+			})
+		})
+
+		function readURL(input) {
+			if (input.files && input.files[0]) {
+				var reader = new FileReader();
+				reader.onload = function (e) {
+					$('.file img').attr('src', e.target.result).css("display", "block");
+				};
+				reader.readAsDataURL(input.files[0]);
+			}
+		}
+
+		$(".modal .input__file").on("change", function () {
+			readURL(this);
+		});
+
+		$(".modal form").submit(function (e) {
+			e.preventDefault()
+			$(this).removeClass("active");
+			$(".send__window").addClass("active");
+			$(".modal .name__input").val("")
+			$(".modal .modal__area").val("")
+			$(".modal .file img").attr("src", "").css("display", "none")
+			delayClose()
+		})
+		function delayClose() {
+			setTimeout(function () {
+				$(".modal form").addClass("active");
+				$(".send__window").removeClass("active");
+				$(".modal .fancybox-close-small").click();
+			}, 5000);
+		}
+	}
+
+	modal()
+
+
+	function carousel(selector, mark) {
+		if ($(window).width() <= 660) {
+			var acarousel = $(selector).acarousel();
+
+			$(selector).swipe({
+				swipeStatus: function (event, phase, direction) {
+					if (phase == "end") {
+						//сработает через 20 пикселей то число которое выбрали в threshold
+						if (direction == 'left') {
+							//сработает при движении влево
+							var move = acarousel.move(-1)
+							changeActive(move);
+						}
+						if (direction == 'right') {
+							//сработает при движении вправо
+							var move = acarousel.move(1);
+							changeActive(move);
+						}
+					}
+				},
+				triggerOnTouchEnd: false,
+				threshold: 20,
+			});
+
+			function changeActive(move) {
+				var index = acarousel.getPos(move).index;
+				$(mark + " .move").removeClass("active").eq(index).addClass("active");
+			}
+			changeActive();
+
+			$(".move").click(function () {
+				if (acarousel.isAnim()) return false;
+				var index = $(".move").index(this);
+				var move = acarousel.moveByIndex(index);
+				changeActive(move);
+				return false;
+			});
+
+			$(window).resize(function () {
+				acarousel.init();
+			});
+		}
+	}
+
+	carousel(".galary__wrapper", ".galary__block .move__mark");
+	carousel(".main__galary-wrapper", ".main__galary .move__mark");
+
 	Date.prototype.daysInMonth = function () {
 		return 32 - new Date(this.getFullYear(), this.getMonth(), 32).getDate();
 	};
@@ -22,9 +137,12 @@ $(function () {
 
 	$("body").on('click', '[href*="#"]', function (e) {
 		var fixedOffset = 0;
+		if ($(document).width() <= 800) {
+			fixedOffset = 400;
+		}
 		$('html,body')
 			.stop()
-			.animate({ scrollTop: $(this.hash).offset().top - fixedOffset }, 1000);
+			.animate({ scrollTop: $(this.hash).offset().top + fixedOffset }, 1000);
 		e.preventDefault();
 	});
 
